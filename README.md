e5b3d6891756ac9ef614a8c4aa4467b941a8b61e
Danilo <danilo.buttafoco@studet.kyh.se>
2019-12-06 13:29:49 +0100

    final quire

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # fe19tp1_Vellum
-Quire_Notebook 
+
+# Quire - A simple note app in your browser!
+
+
+Quire is a simple note taking app in the browser. Mobile-friendly, local storage, fast and responsive.  
+
+  - Write,
+  - Save,
+  - Delete Documents
+
+# Features!
+
+  - Write a document and it will save automatically.
+  - Add favorites to find them more easily.
+
+You can also:
+  - Add images to your documents. 
+  - Change it to darkmode.
+  - Easy to print document. 
+
+
+# Tech
+
+#### JavaScript
+
+The code is writen in structred and oriented way. There is only one JS file in the project. Quill editor library is linked at line 58 in the HTML. The project has sevreal different functions, that can save, add, remove, search and favorite.
+
+The global variables are delared at the top of app.js.
+
+#### HTML 
+
+The HTML is very simple and easy to read single page app, there is only one HTML document in this project. 
+
+#### CSS
+
+The CSS file contains the styling rules for both desktop and mobile versions of the app. 
+Everything in the media query block decides the design for mobile view.
+
+
+# Project members 
+- Danilo Buttafoco
+- Tom Ederstål
+- Gustaf Ekelund
+- Phani Kumar Kotikalapudi
+- Ishrath Ansurudeen
--- app.js
+++ app.js
@@ -1,76 +1,195 @@
 let noteList = [];
-const savebtn = document.querySelector("#sideBar > i.fas.fa-plus")
-const justText = document.querySelector('#justText');
-const input = document.querySelector('#search');
+const newDoc = document.querySelector(".fa-plus");
+const noteContent = document.querySelector('#noteContent');
+const input = document.querySelector('.search');
 const saveToFav = document.querySelector('#saveFav');
-const favIcon = document.querySelector("#sideBar > i.far.fa-heart");
-let selected;
+const favIcon = document.querySelector(".fa-heart");
+const print = document.querySelector("#print")
+const openSideBar = document.querySelector('.fa-folder');
+const openSideBarLogo = document.querySelector('.logo');
+const closeBtn = document.querySelector('.closebtn');
+const darkMode = document.querySelector('.fa-moon');
+const changeEditor = document.querySelector('#editor');
+let selectedNote;
+let clicks = 1;
 
-// Sidabar
-function openNav() {
-  document.getElementById("secondSideBar").style.width = "300px";
-  document.getElementById("editor").style.width = "70%";
-  document.getElementById("editor").style.marginLeft = "420px";
-}
-function closeNav() {
-  document.getElementById("secondSideBar").style.width = "0";
-  document.getElementById("editor").style.width = "90%";
-  document.getElementById("editor").style.marginLeft = "130px";
-}
+
+/* Sidebar toggle */
+const SecondSideBarToggle = () => {
+  const secondSideBarContainer = document.querySelector('.secondSideBarContainer');
+  // Toggle Nav
+  openSideBar.addEventListener('click', () => {
+    secondSideBarContainer.classList.toggle('secondSideBarContainer-active');
+  });
+}; SecondSideBarToggle();
+
+
 /* Editor */
 
 var Delta = Quill.import('delta');
 var options = {
   modules: {
-    toolbar: [
-      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-      ['bold', 'italic', 'underline'],
-      ['link', 'image'],
-      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-      [{ 'align': [] }],
-    ]
+    toolbar:
+      [
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        ['bold'],
+        ['italic'],
+        ['underline'],
+        ['link'],
+        ['image'],
+        [{ 'list': 'ordered' }],
+        [{ 'list': 'bullet' }],
+        [{ 'align': [] }],
+      ]
   },
   placeholder: 'Write something fun...',
+  scrollingContainer: '#mainEditorContainer',
   theme: 'snow'
 };
 
+var icons = Quill.import('ui/icons');
+
 var editor = new Quill('#quillEditor', options);
 
-//Save button
-savebtn.addEventListener('click', () => {
+editor.on('text-change', function (delta) {
+  if (!selectedNote) {
+    selectedNote = newNote();
+  }
 
-  let note = {
-    id: Date.now(),
-    title: editor.getText(0, 10),
-    contents: editor.getContents(),
-    favourite: false,
-    started: true
-  };
+  let contents = editor.getContents();
+  let save = JSON.stringify(contents, null, 2);
 
-  createDiv(note);
-  noteList.unshift(note);
-  saveNotes();
+  if (delta) {
+    save = JSON.stringify(delta, null, 2) + "\n\n" + save;
+    selectedNote.contents = contents;
+    selectedNote.title = editor.getText(0, 10);
+
+    if (noteList.filter(note => note.id == selectedNote.id).length < 1) {
+      noteList.unshift(selectedNote);
+      createDiv(selectedNote);
+      saveNotes()
+    } else {
+      if (selectedNote.deleted === false) {
+        selectedNote.contents = contents;
+        selectedNote.title = editor.getText(0, 10);
+        saveNotes();
+        updateDiv(selectedNote);
+      }
+    }
+  }
+});
+
+//Click event för divarna
+noteContent.addEventListener('click', e => {
+  let noteID = e.target.closest("div").id;
+  selectedNote = noteList.find(note => note.id === Number(noteID));
+
+  if (e.target.classList.contains("delete")) { del(e) }
+
+
+  if (e.target.classList.contains('far')) { selectFavo(e) }
+  else { pickDoc(e) } //clicked div
+})
+
+//Keyup event för search
+input.addEventListener('keyup', e => {
+  search();
+  input.addEventListener('click', e => {
+    noteContent.childNodes.forEach(e => { e.style.display = "block" })
+  })
+})
+
+//Favoritikonens toggle effekt 
+favIcon.addEventListener('click', e => {
+  let arrayOfDivs = Array.from(noteContent.childNodes);
+
+  clicks += 1;
+
+  arrayOfDivs.forEach(div => {
+    let noteID = div.id;
+    selectedNote = noteList.find(note => note.id === Number(noteID));
+    if (clicks % 2 === 0) {
+      if (selectedNote.favourite) {
+        div.style.display = "block";
+      } else { div.style.display = "none" }
+    } else { div.style.display = "block" }
+
+  })
+})
+
+//nytt dokumentknapp (+)
+newDoc.addEventListener('click', () => {
+  Array.from(noteContent.childNodes).map(div => { div.classList.remove('picked') })
+  if (selectedNote) {
+    selectedNote = !selectedNote;
+  }
   clearEditor();
 });
 
+//skapar en ny note med "keys" som innehåller id, editor content mm. 
+function newNote() {
+  return {
+    id: Date.now(),
+    favourite: false,
+    contents: "notes",
+    title: "title",
+    deleted: false
+  };
+}
+
+
+// Tar bort noten   
+function del(e) {
+  selectedNote.deleted = true;
+  e.target.closest('div').remove();
+  saveNotes();
+  clearEditor();
+  selectedNote = !selectedNote
+}
+
+
+// skapar en div i second sidebar med info om noten
 function createDiv(note) {
   let newDiv = document.createElement("div");
   newDiv.id = note.id;
   newDiv.classList.add('div');
+  if (selectedNote && selectedNote.id == note.id) {
+    newDiv.classList.add("picked")
+  }
+
   let newDivList = {
-    title: `<strong>Title:</strong><span>${note.title}....</span>`,
-    date: `<strong>Datum:</strong><span>${Date(note.id)}</span>`,
-    icon: `<span class='far fa-heart ${note.favourite ? 'fas fa-heart' : ''}'></span>`
+    title: `<strong>${note.title}</strong>`,
+    date: `<span>${Date(note.id)}</span>`,
+    icon: `<span class='far fa-heart ${note.favourite ? 'fas fa-heart' : ''}'></span>`,
+    trash: `<span class='far fa-trash-alt delete'></span>`
   };
-  newDiv.innerHTML = ` ${newDivList.title} <br> ${newDivList.date} ${newDivList.icon}`;
-  
-  if(document.readyState === "complete" || document.readyState === "loaded"){
-    justText.insertBefore(newDiv, justText.childNodes[0])
-  } else {justText.appendChild(newDiv)}
+  newDiv.innerHTML = ` ${newDivList.title} ${newDivList.trash} ${newDivList.icon} <br> ${newDivList.date};`
+  noteContent.appendChild(newDiv);
+}
+
+//updaterar en div. tarbort gamla oh byter ut den till en ny.
+function updateDiv(note) {
+
+  let oldDiv = document.getElementById(note.id);
+  let newDiv = document.createElement("div");
+  newDiv.id = note.id;
+  newDiv.classList.add('div');
+  if (selectedNote && selectedNote.id == note.id) {
+    newDiv.classList.add('picked')
+  }
+  let newDivList = {
+    title: `<strong>${note.title}</strong>`,
+    date: `<span>${Date(note.id)}</span>`,
+    icon: `<span class='far fa-heart ${note.favourite ? 'fas fa-heart' : ''}'></span>`,
+    trash: `<span class='far fa-trash-alt delete'></span>`
+  };
+  newDiv.innerHTML = ` ${newDivList.title} ${newDivList.trash} ${newDivList.icon} <br> ${newDivList.date};`
+  noteContent.replaceChild(newDiv, oldDiv)
+
 }
 
 // Load localstorage när sidan laddar
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('load', (event) => {
   let quire = JSON.parse(localStorage.getItem('quire'));
   if (quire) {
     modal.style.display = "none";
@@ -84,7 +203,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 function loadNotes() {
   if (localStorage.getItem('quire')) {
     noteList = JSON.parse(localStorage.getItem('quire')).notes
-    editor.setContents(noteList[0].contents);
+    //editor.setContents(noteList[0].contents);
     renderDocs();
   } else {
     noteList = [];
@@ -106,54 +225,78 @@ function saveNotes() {
 
 //Renderar dokumentet samt div när sidan laddas om
 function renderDocs() {
-  noteList.forEach(note => createDiv(note));
+  noteList.forEach(note => {
+    if (note.deleted === false) {
+      createDiv(note)
+    }
+  })
 }
 
-//Click event för divarna
-justText.addEventListener('click', e => {
-  const newDiv = document.querySelectorAll('#justText > div');
+//laddar data från localstorage och läggertillbaka data i editorn när en av note-divarna klickas. Sätter en ny class på noten som klickats. 
+function pickDoc(e) {
   const loadData = JSON.parse(localStorage.getItem('quire')).notes;
-  let noteID = e.target.closest("div").id;
-  selectedNote = noteList.find(note => note.id === Number(noteID));
-  
-  if (e.target.classList.contains('far')) {
-    selectedNote.favourite = !selectedNote.favourite
-    saveNotes();
-    e.target.classList.toggle('fas'); 
-
-  } else {
-    for (let i = 0; i < loadData.length; i++) {
-      if (loadData[i].id == e.target.parentElement.id) {
-        editor.setContents(loadData[i].contents);
-        newDiv.forEach(event => {
-          if (e.target.parentElement === event || e.target === event) {
-            event.classList.add('picked');
-          } else { event.classList.remove('picked') }
-        })
-      }
+  const newDiv = document.querySelectorAll('#noteContent > div');
+  for (let i = 0; i < loadData.length; i++) {
+    if (loadData[i].id == e.target.closest('div').id) {
+      editor.setContents(loadData[i].contents);
+      newDiv.forEach(event => {
+        if (e.target.closest('div') == event) {
+          event.classList.add('picked');
+        } else { event.classList.remove('picked'); }
+      })
     }
   }
-}) 
-
+}
+//Söker igenom divarna i second sidebar. Söker inbart i rubrikerna.
 function search() {
   let input = document.querySelector('#search');
   let filter = input.value.toUpperCase();
-  for (let i = 0; i < justText.childNodes.length; i++) {
-    textValue = justText.childNodes[i].childNodes[2].textContent || justText.childNodes[i].childNodes[2].innerText;
+  for (let i = 0; i < noteContent.childNodes.length; i++) {
+    textValue = noteContent.childNodes[i].childNodes[1].textContent || noteContent.childNodes[i].childNodes[1].innerText;
     if (textValue.toUpperCase().indexOf(filter) > -1) {
-      justText.childNodes[i].style.display = "";
+      noteContent.childNodes[i].style.display = "";
     } else {
-      justText.childNodes[i].style.display = 'none';
+      noteContent.childNodes[i].style.display = 'none';
     }
   }
 }
 
-input.addEventListener('keyup', e => {
-  search();
-  input.addEventListener('click', e => {
-    justText.childNodes.forEach(e => { e.style.display = "block" })
-  })
-})
+//Sätt en note till favorit eller ta bort som favorit.
+function selectFavo(e) {
+  selectedNote.favourite = !selectedNote.favourite
+  saveNotes();
+  e.target.classList.toggle('fas');
+}
+
+
+//Print-funktion 
+function printDiv() {
+  var divContents = document.querySelector(".ql-editor").innerHTML;
+  var a = window.open('', '', 'height=1200, width=1200');
+  a.document.write('<html>');
+  a.document.write('<body > ');
+  a.document.write(divContents);
+  a.document.write('</body></html>');
+  a.document.close();
+  a.print();
+}
+
+function myFunction() {
+  var element = document.body;
+  var secondSideBar = document.querySelector('.secondSideBar');
+  var secondSideBarContainer = document.querySelector('.secondSideBarContainer');
+  var darkEditor = document.querySelector(".ql-editor");
+
+  darkEditor.classList.toggle("dark-mode-editor");
+  element.classList.toggle("dark-mode");
+  secondSideBar.classList.toggle("dark-mode-secondSidebar");
+  secondSideBarContainer.classList.toggle("dark-mode-secondSidebar");
+  input.classList.toggle("dark-mode-secondSidebar");
+  noteContent.classList.toggle("dark-mode-secondSidebar");
+}
+
+
+
 
 /* Popup */
 
@@ -179,45 +322,8 @@ window.onclick = function (event) {
   }
 }
 
-function displayChecked() {
-  let checkboxes = document.querySelectorAll('.fav > #justText > div > input');
-  let arrayOfCheckboxes = Array.from(checkboxes);
-  arrayOfCheckboxes.map(e => {
-    if (e.checked) { e.parentElement.style.display = 'block' }
-    else { e.parentElement.style.display = 'none' }
-  })
-}
-
-let clicks = 0;
-favIcon.addEventListener('click', e => {
-  let arrayOfDivs = Array.from(justText.childNodes);
-  clicks += 1;
-  
-
-  console.log(clicks);
-  arrayOfDivs.forEach(div => {
-    let noteID = div.id;
-    selectedNote = noteList.find(note => note.id === Number(noteID));
-    if(clicks % 2 === 0) {
-    if(selectedNote.favourite){
-      div.style.display = "block"
-    } else { div.style.display = "none"}
-    } else {div.style.display ="block"}
-
-  })
-})
-
-function printDiv() {
-  var divContents = document.querySelector(".ql-editor").innerHTML;
-  var a = window.open('', '', 'height=1200, width=1200');
-  a.document.write('<html>');
-  a.document.write('<body > ');
-  a.document.write(divContents);
-  a.document.write('</body></html>');
-  a.document.close();
-  a.print();
-}
 
 print.addEventListener('click', () => {
   printDiv();
 })
+
